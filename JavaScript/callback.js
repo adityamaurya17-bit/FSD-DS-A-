25e17cc117503e99e06720a13fc823ffe92c0d18
Aditya Maurya <adityamaurya092004@gmail.com>
2026-08-21 10:27:48 +0530

    Callback in js

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,9 +1,25 @@
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+// function sumWithMsg(clbk, msg){
+//     const result = clbk(20,30);
+//     const fresult = "Hi! " + msg + ", your score is: "+ result;
+//     console.log(fresult);
+// }
+// sumWithMsg(sum, "Mr. Deepak");
+
+function authorize(name, password){
+    if (name==="Aditya" && password==="admin123"){
+        return "authorized";
+    } else {
+        return "unauthorized";
+    }
 }
-function sumWithMsg(clbk, msg){
-    const result = clbk(20,30);
-    const fresult = "Hi! " + msg + ", your score is: "+ result;
-    console.log(fresult);
+
+function authorizeWithMsg(clbk, msg){
+    const result = clbk("Rohit", "admin123");
+    
+    const finalresult = "Hi! " + msg + ", "+ result;
+    console.log(finalresult);
 }
-sumWithMsg(sum, "Mr. Deepak");
+authorizeWithMsg(authorize, "You are");
